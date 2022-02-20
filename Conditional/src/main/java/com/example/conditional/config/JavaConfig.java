b3c56c3f5ba0b82450d1d97e1bad622978fa003e
Dvornikov <dvotnikov90@yandex.ru>
2022-02-20 19:10:25 +0300

    Homework

---
+++ Conditional/src/main/java/com/example/conditional/config/JavaConfig.java
@@ -0,0 +1,23 @@
+package com.example.conditional.config;
+
+import com.example.conditional.profile.DevProfile;
+import com.example.conditional.profile.ProductionProfile;
+import com.example.conditional.profile.SystemProfile;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false", matchIfMissing = true)
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
